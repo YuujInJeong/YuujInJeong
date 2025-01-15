d46aebc6ed096fe0e9c79ae26b1043ee536a6b7c
yujinjeong0917 <yujin010917@khu.ac.kr>
2025-01-15 15:42:33 +0900

    Update README automation script and workflow

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -1,48 +1,44 @@
 import { readFileSync, writeFileSync } from "node:fs";
 import Parser from "rss-parser";
 
-// 기존 README.md 파일 읽기
 const readmePath = "README.md";
 let readmeContent = readFileSync(readmePath, "utf8");
+const parser = new Parser();
 
-// RSS 파서 생성
-const parser = new Parser({
-    headers: {
-        Accept: "application/rss+xml, application/xml, text/xml; q=0.1",
-    },
-});
-
-// 최신 블로그 포스트 추가하는 함수
 (async () => {
-    // 1. Tistory RSS 피드 가져오기
-    const tistoryFeed = await parser.parseURL("https://realalpaca01.tistory.com/rss"); // Tistory RSS URL
+    // RSS 피드 가져오기
+    const tistoryFeed = await parser.parseURL("https://realalpaca01.tistory.com/rss");
+    const velogFeed = await parser.parseURL("https://v2.velog.io/rss/@yujin_jeong");
+
+    // 최신 블로그 포스트 생성
     let tistoryPosts = "### Tistory Latest Blog Posts\n\n";
     for (let i = 0; i < 5 && i < tistoryFeed.items.length; i++) {
         const { title, link } = tistoryFeed.items[i];
         tistoryPosts += `- [${title}](${link})\n`;
     }
 
-    // 2. Velog RSS 피드 가져오기
-    const velogFeed = await parser.parseURL("https://v2.velog.io/rss/@yujin_jeong"); // Velog RSS URL
     let velogPosts = "### Velog Latest Blog Posts\n\n";
     for (let i = 0; i < 5 && i < velogFeed.items.length; i++) {
         const { title, link } = velogFeed.items[i];
         velogPosts += `- [${title}](${link})\n`;
     }
 
-    // 3. 기존 README.md에 최신 블로그 포스트 추가
     const combinedPosts = `${tistoryPosts}\n${velogPosts}`;
+
+    // README.md 업데이트 여부 확인
     const newReadmeContent = readmeContent.includes("### Tistory Latest Blog Posts")
         ? readmeContent.replace(
-            /### Tistory Latest Blog Posts[\s\S]*?(?=### Velog Latest Blog Posts|$)/,
-            tistoryPosts + "\n" + velogPosts
-        )
+              /### Tistory Latest Blog Posts[\s\S]*?(?=### Velog Latest Blog Posts|$)/,
+              combinedPosts
+          )
         : readmeContent + "\n" + combinedPosts;
 
     if (newReadmeContent !== readmeContent) {
         writeFileSync(readmePath, newReadmeContent, "utf8");
         console.log("README.md 업데이트 완료");
+        process.exit(0); // 정상적으로 종료 (업데이트 있음)
     } else {
-        console.log("새로운 블로그 포스트가 없습니다. README.md 파일이 업데이트되지 않았습니다.");
+        console.log("새로운 블로그 포스트가 없습니다.");
+        process.exit(1); // 업데이트 없음
     }
 })();
